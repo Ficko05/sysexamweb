30c1c3d107f57fd73b121b5db4b46375e7b5cc6a
Steven <Steven-ptkhuu@hotmail.com>
2018-12-04 15:27:22 +0100

    style and Css from Gasser(outsourings)

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Navigation from "./Navigation.js";
-import Login from "./login.js";
+import Login from "./Login.js";
 import Home from "./Home.js";
 import ShowHotels from "./ShowHotels.js";
 import Social from "./Social.js";
@@ -13,13 +13,20 @@ export default class Router extends Component {
             <HashRouter>
                 <div>
                     <Navigation />
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route path="/login" component={Login} />
-                        <Route path="/hotels" component={ShowHotels}/>
-                        <Route path="/social" component={Social}/>
-                        <Route path="/search" component={search}/>
-                    </Switch>
+                    <div className="content my-3">
+                        <div className="row" style={{ margin: "0" }}>
+                            <div className="col-7 col-sm-8 col-md-9 col-lg-10 p-3 maincontent">
+                                
+                                <Switch>
+                                    <Route exact path="/" component={Home} />
+                                    <Route path="/login" component={Login} />
+                                    <Route path="/hotels" component={ShowHotels} />
+                                    <Route path="/social" component={Social} />
+                                    <Route path="/search" component={search} />
+                                </Switch>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </HashRouter>
         );
