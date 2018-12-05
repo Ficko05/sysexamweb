22d6f551af26b2d24c8992b2d8ade17efb1e4a2f
Ionsight <ionsight@users.noreply.github.com>
2018-12-05 15:57:23 +0100

    removed registration fetch call from Home

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,5 @@
 import React from "react";
-import facade from './apiFacade.js';
 const Home = () => {
-    facade.registration("bo", "kevin")
     return ( 
     <div className="content my-3">
         <div className="row" style={{margin: "0"}}>
